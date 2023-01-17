c97831bc9347323a381811039a2d6f48e2d553dd
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-17 16:28:02 -0300

    constructor model #1

--- src/FipeLib/Model/TabelaReferenciaModel.cs
+++ src/FipeLib/Model/TabelaReferenciaModel.cs
@@ -3,10 +3,16 @@ namespace FipeLib.Model;
 public class TabelaReferenciaModel
 {
     public int Codigo { get; }
-    public string Mes { get; } = string.Empty;
+    public string Mes { get; }
 
     public TabelaReferenciaModel(int codigo, string mes)
     {
+        if (codigo == 0)
+            throw new ArgumentException(nameof(codigo));
+
+        if (string.IsNullOrEmpty(mes))
+            throw new ArgumentException(nameof(mes));
+            
         Codigo = codigo;
         Mes = mes;
     }
