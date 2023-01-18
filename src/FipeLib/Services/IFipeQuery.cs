0a87db34a369f72463bdc31aa37b448acfe2e589
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-18 11:03:48 -0300

    ConsultarMarcas #2 - doc

--- src/FipeLib/Services/IFipeQuery.cs
+++ src/FipeLib/Services/IFipeQuery.cs
@@ -4,10 +4,33 @@ namespace FipeLib.Services;
 
 public interface IFipeQuery
 {
+    /// <summary>
+    /// Get async enumerable of 'Tabela Referencia'
+    /// </summary>
     Task<IEnumerable<TabelaReferenciaModel>> GetAllTabelaReferenciaAsync();
+
+    /// <summary>
+    /// Get enumerable of 'Tabela Referencia'
+    /// </summary>
     IEnumerable<TabelaReferenciaModel> GetAllTabelaReferencia();
+
+    /// <summary>
+    /// Get 'marcas' in current date of 'Tabela Referencia'
+    /// </summary>
     IEnumerable<MarcaModel> GetMarca();
+
+    /// <summary>
+    /// Get async 'marcas' in current date of 'Tabela Referencia'
+    /// </summary>
     Task<IEnumerable<MarcaModel>> GetMarcaAsync();
+
+    /// <summary>
+    /// Get 'marcas' with specific 'Tabela Referencia'
+    /// </summary>
     IEnumerable<MarcaModel> GetMarca(TabelaReferenciaModel? tabelaReferenciaModel);
+    
+    /// <summary>
+    /// Get async 'marcas' with specific 'Tabela Referencia'
+    /// </summary>
     Task<IEnumerable<MarcaModel>> GetMarcaAsync(TabelaReferenciaModel? tabelaReferenciaModel);
 }
